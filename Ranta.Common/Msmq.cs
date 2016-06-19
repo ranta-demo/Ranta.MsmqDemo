b61f8b290e22b0f1b5a84f2ef6527ae898df53ee
HarrisZhang <harriszhang@live.cn>
2016-06-19 16:37:24 +0800

    init project

---
+++ Ranta.Common/Msmq.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+
+namespace Common
+{
+	public class Msmq<T> where T : class,new()
+	{
+		public Msmq(string path)
+		{
+			try
+			{
+				if (MessageQueue.Exists(path))
+				{
+					this.queue = new MessageQueue(path);
+				}
+				else
+				{
+					this.queue = MessageQueue.Create(path);
+				}
+				this.queue.Formatter = new BinaryMessageFormatter();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
+		private MessageQueue queue;
+
+		public void Enqueue(T t)
+		{
+			try
+			{
+				this.queue.Send(t);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
+		public T Dequeue()
+		{
+			T t = default(T);
+
+			try
+			{
+				t = this.queue.Receive().Body as T;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+
+			return t;
+		}
+	}
+}
---
+++ Ranta.Enqueue/EnqueueProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common;
+
+namespace Enqueue
+{
+	class EnqueueProgram
+	{
+		static void Main(string[] args)
+		{
+			Msmq<Student> queue = new Msmq<Student>(AppConst.QueuePath);
+
+			queue.Enqueue(new Student { Name = "Tom", Age = 18 });
+
+			//var student = queue.Dequeue();
+
+			//if (student != null)
+			//{
+			//	Console.WriteLine(student.Name);
+			//	Console.WriteLine(student.Age);
+			//}
+			//else
+			//{
+			//	Console.WriteLine("student is null.");
+			//}
+
+			Console.WriteLine("Press any key to continue.");
+			Console.Read();
+		}
+	}
+}
